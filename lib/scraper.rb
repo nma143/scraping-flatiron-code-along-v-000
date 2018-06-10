96a574f228d954513d91166287ebe56db8b5eff0
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-06-10 22:47:39 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,6 +18,8 @@ class Scraper
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
     binding.pry
+    #course name: doc.css(".post").first.css("h2").text
+
   end
 end
 Scraper.new.get_page
