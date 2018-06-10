9573ab9c352315ff7e2672430d5e17dc72a943cb
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-06-10 22:20:07 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,15 +1,19 @@
 class Course
 
-@@all = []
-attr_accessor :title, :schedule, :description
+  @@all = []
+  attr_accessor :title, :schedule, :description
 
-def initialize
+  def initialize
 
-  @@all << self
-end
+    @@all << self
+  end
 
-def self.all
-  @@all
-end
+  def self.all
+     @@all
+   end
+
+   def self.reset_all
+     @@all.clear
+   end
 
 end
